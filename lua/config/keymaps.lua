831b6ddf51759c4b2f7b4e020c617ffaa9734b69
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2024-09-20 08:26:13 +0200

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -66,11 +66,11 @@ vim.keymap.set(
 )
 
 -- Custom copilot accepting keymap
-vim.keymap.set("i", "<CR>", function()
+vim.keymap.set("i", "<Tab>", function()
   if require("copilot.suggestion").is_visible() then
     require("copilot.suggestion").accept()
   else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
   end
 end, {
   silent = true,
