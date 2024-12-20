71ed92593689e56e7d5be8d59292626cb995e1d1
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2024-12-20 11:57:14 +0100

    config adjustments after upgrade to v14

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,11 +1,25 @@
 return {
   -- disable copilot-cmp
-  { "zbirenbaum/copilot-cmp", enabled = false },
   { "AndreM222/copilot-lualine" },
-  { "nvim-lualine/lualine.nvim" },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(
+        opts.sections.lualine_x,
+        2,
+        LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+          local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+          if #clients > 0 then
+            local status = require("copilot.api").status.data.status
+            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+          end
+        end)
+      )
+    end,
+  },
   {
     "zbirenbaum/copilot.lua",
-    -- event = "VeryLazy",
     cmd = "Copilot",
     build = ":Copilot auth",
     event = "InsertEnter",
